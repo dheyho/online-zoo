7bde858b772d1d210bca5c93dc7fe0988b59506a
dheyho <dheyho@mail.ru>
2021-05-06 19:56:24 +0300

    refactor: code in script1

--- script1.js
+++ script1.js
@@ -21,32 +21,32 @@ let sideBar = document.querySelector('.side-bar');
 var flagForButton = true;
 
 function swichAsideTrue() {
-sideBar.style.width = "350px";
-sideCardSpan.forEach(element => {
-element.style.margin = "20px";
-});
-sideCardSpan.forEach(element => {
-element.style.display = "block";
-});
-flagForButton = false;
-sideBtn.style.transform = 'rotate(180deg)';
+    sideBar.style.width = "350px";
+    sideCardSpan.forEach(element => {
+        element.style.margin = "20px";
+    });
+    sideCardSpan.forEach(element => {
+        element.style.display = "block";
+    });
+    flagForButton = false;
+    sideBtn.style.transform = 'rotate(180deg)';
 }
 function swichAsideFalse() {
-sideBar.style.width = "220px";
-sideCardSpan.forEach(element => {
-element.style.margin = "20px";
-});
-sideCardSpan.forEach(element => {
-element.style.display = "none";
-});
-flagForButton = true;
-sideBtn.style.transform = 'rotate(0deg)';
+    sideBar.style.width = "220px";
+    sideCardSpan.forEach(element => {
+        element.style.margin = "20px";
+    });
+    sideCardSpan.forEach(element => {
+        element.style.display = "none";
+    });
+    flagForButton = true;
+    sideBtn.style.transform = 'rotate(0deg)';
 }
 sideBtn.addEventListener('click', function (e) {
-if (flagForButton == true) {
-swichAsideTrue();
-}
-else {
-swichAsideFalse();
-}
+    if (flagForButton == true) {
+        swichAsideTrue();
+    }
+    else {
+        swichAsideFalse();
+    }
 })
